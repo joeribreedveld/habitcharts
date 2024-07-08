668ca1556650163cdebc85a7d7605b8dcbca51f3
joeribreedveld <joeri.breedveld123@gmail.com>
2024-07-08 19:43:06 +0200

    refactor: prettier import sorter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
 type TLayout = Readonly<{
   children: React.ReactNode;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,41 @@
+import Habit from "@/components/habit";
+
 export default function Page() {
-  return <main>Hello, World!</main>;
+  return (
+    <main className="container py-16">
+      <div className="grid grid-cols-1 gap-4 sm:gap-6 md:grid-cols-2 xl:grid-cols-3">
+        <Habit theme="red" title="Title" description="Description" target={5} />
+        <Habit
+          theme="orange"
+          title="Title"
+          description="Description"
+          target={5}
+        />
+        <Habit
+          theme="green"
+          title="Title"
+          description="Description"
+          target={5}
+        />
+        <Habit
+          theme="blue"
+          title="Title"
+          description="Description"
+          target={5}
+        />
+        <Habit
+          theme="yellow"
+          title="Title"
+          description="Description"
+          target={5}
+        />
+        <Habit
+          theme="violet"
+          title="Title"
+          description="Description"
+          target={5}
+        />
+      </div>
+    </main>
+  );
 }
