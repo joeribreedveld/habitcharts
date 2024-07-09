fdeb98204325634e960a88a66cc4c96df3adc376
joeribreedveld <joeri.breedveld123@gmail.com>
2024-07-09 18:12:50 +0200

    fix: try deployment issue by removing iso date

--- components/habit.tsx
+++ components/habit.tsx
@@ -54,7 +54,7 @@ export default function Habit({
   async function handleToggleRecord() {
     setIsLoading(true);
 
-    await toggleRecord(id, new Date().toISOString());
+    await toggleRecord(id, new Date().toDateString());
 
     setIsLoading(false);
   }
