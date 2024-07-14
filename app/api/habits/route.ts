51cd03df24117be8fcd316d42236fe71025c804f
joeribreedveld <joeri.breedveld123@gmail.com>
2024-07-14 11:54:50 +0200

    fix: this fix?

--- app/api/habits/route.ts
+++ app/api/habits/route.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET(request: Request) {
+export async function GET() {
   const response = await prisma.habit.findMany();
 
   if (!response) {
