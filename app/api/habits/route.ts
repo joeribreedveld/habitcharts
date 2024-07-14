4d8b919a10db837144867bdf410b84c17b5fedb5
joeribreedveld <joeri.breedveld123@gmail.com>
2024-07-14 10:46:04 +0200

    fix: log response for deploy error

--- app/api/habits/route.ts
+++ app/api/habits/route.ts
@@ -5,5 +5,7 @@ const prisma = new PrismaClient();
 export async function GET(request: Request) {
   const response = await prisma.habit.findMany();
 
+  console.log(response);
+
   return Response.json({ habits: response });
 }
