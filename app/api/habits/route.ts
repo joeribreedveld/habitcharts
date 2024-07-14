294c1e0d9da5f1f2122e91f3ad01a631aab31b74
joeribreedveld <joeri.breedveld123@gmail.com>
2024-07-14 11:46:34 +0200

    fix: maybe this works

--- app/api/habits/route.ts
+++ app/api/habits/route.ts
@@ -5,6 +5,14 @@ const prisma = new PrismaClient();
 export async function GET(request: Request) {
   const response = await prisma.habit.findMany();
 
+  if (!response) {
+    return new Response(JSON.stringify({ habits: [] }), {
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  }
+
   return new Response(JSON.stringify({ habits: response }), {
     headers: {
       "content-type": "application/json",
