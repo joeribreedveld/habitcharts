1bed808ec60063c2b0d553cd9f636efbf077cb6d
joeribreedveld <joeri.breedveld123@gmail.com>
2024-07-14 11:18:19 +0200

    fix: forgot https

--- lib/utils/getBaseUrl.ts
+++ lib/utils/getBaseUrl.ts
@@ -1,5 +1,5 @@
 export function getBaseUrl() {
   return process.env.NODE_ENV === "production"
-    ? process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
     : "http://localhost:3000";
 }
