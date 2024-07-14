0366f6a43198111121ac3c55f7e6a7afa1744d94
joeribreedveld <joeri.breedveld123@gmail.com>
2024-07-14 11:58:56 +0200

    fix: another test

--- app/api/habits/route.ts
+++ app/api/habits/route.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET() {
+export async function GET(request: Request) {
   const response = await prisma.habit.findMany();
 
   if (!response) {
@@ -19,3 +19,5 @@ export async function GET() {
     },
   });
 }
+
+export const dynamic = "force-dynamic";
