87b5e739f3d5364d5bf031f2383fa9e80b9cf1d9
joeribreedveld <joeri.breedveld123@gmail.com>
2024-07-18 09:22:19 +0200

    fix: remove test record delay

--- lib/utils/habits/getRecords.ts
+++ lib/utils/habits/getRecords.ts
@@ -3,8 +3,6 @@ import { unstable_noStore } from "next/cache";
 export async function getRecords(habitId: string) {
   unstable_noStore();
 
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_URL}/api/habits/${habitId}/records`,
     {
