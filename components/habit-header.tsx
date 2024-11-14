d8b399386ed3f4bf4290f566a42ac6675b06723d
joeribreedveld <joeri.breedveld123@gmail.com>
2024-11-14 13:29:33 +0100

    feat: responsive improvements cards

--- components/habit-header.tsx
+++ components/habit-header.tsx
@@ -2,6 +2,7 @@
 
 import HabitCreate from "@/components/habit-create";
 import { Button } from "@/components/ui/button";
+import { CircleHelpIcon } from "lucide-react";
 import { useState } from "react";
 
 export default function HabitHeader() {
@@ -10,7 +11,10 @@ export default function HabitHeader() {
   return (
     <header className="flex flex-col items-start justify-between gap-6 sm:flex-row sm:items-center px-4 mt-12 sm:container">
       <div>
-        <h1 className="text-lg font-semibold">Habitcharts</h1>
+        <div className="flex gap-3 items-center">
+          <h1 className="text-lg font-semibold">Habitcharts</h1>
+          {/* <CircleHelpIcon className="w-5 h-5 text-muted-foreground" /> */}
+        </div>
         <p className="text-muted-foreground">
           Track your habits and reach your goals
         </p>
